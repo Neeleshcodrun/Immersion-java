2234a695136e5d479042b5884892805899ff65ac
Neeleshcodrun <156096773+Neeleshcodrun@users.noreply.github.com>
2026-06-16 13:48:09 +0530

    Add files via upload

---
+++ Encap.java
@@ -0,0 +1,35 @@
+public class Encap{
+    private int age;
+    private String name;
+    public Encap(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
+
+@SuppressWarnings("unused")
+class mohit{
+
+    public static void main(String[] args) {
+        Encap e1=new Encap("neelesh",22);
+        System.out.println(e1.getAge());
+        System.out.println(e1.getName());
+    }
+}
